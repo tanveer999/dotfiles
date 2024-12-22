b54a4f481caf0367c3df84ddef576fc78515e451
Tanveer Ahmed <tanveer@test.com>
2024-12-22 15:52:43 +0530

    nvim neo-tree show hidden

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -8,6 +8,15 @@ return {
   },
   config = function()
     vim.keymap.set('n','<C-b>',':Neotree filesystem reveal left<CR>')
+    require('neo-tree').setup({
+      filesystem = {
+        filtered_items = {
+          visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
+          hide_dotfiles = false,
+          hide_gitignored = true,
+        }
+      }
+    })
   end
 }
 
