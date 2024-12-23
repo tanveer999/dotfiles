41f1d7b529c7f92c444feb85cb0bc92f48d318c7
Tanveer Ahmed <tanveer@test.com>
2024-12-23 19:38:25 +0530

    nvim lsp config code action

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,53 +1,70 @@
 return {
-  'nvim-telescope/telescope.nvim',
-  tag = '0.1.8',
-  dependencies = { 'nvim-lua/plenary.nvim' },
-  config = function()
-    -- Telescope
-    local builtin = require('telescope.builtin')
-    vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-    vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-    vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-    vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
-    vim.keymap.set('n', '<leader>fgs', builtin.git_status, { desc = 'Telescope git status' })
+  {
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.8',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      -- Telescope
+      local builtin = require('telescope.builtin')
+      vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+      vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+      vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+      vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+      vim.keymap.set('n', '<leader>fgs', builtin.git_status, { desc = 'Telescope git status' })
 
-    require('telescope').setup({
-      defaults = {
-        -- configure to use ripgrep
-        vimgrep_arguments = {
-          "rg",
-          "--follow",        -- Follow symbolic links
-          "--hidden",        -- Search for hidden files
-          "--no-heading",    -- Don't group matches by each file
-          "--with-filename", -- Print the file path with the matched lines
-          "--line-number",   -- Show line numbers
-          "--column",        -- Show column numbers
-          "--smart-case",    -- Smart case search
-
-          -- Exclude some patterns from search
-          "--glob=!**/.git/*",
-          "--glob=!**/.idea/*",
-          "--glob=!**/.vscode/*",
-          "--glob=!**/build/*",
-          "--glob=!**/dist/*",
-          "--glob=!**/yarn.lock",
-          "--glob=!**/package-lock.json",
-        },
-      },
-      pickers = {
-        find_files = {
-          hidden = true,
-          find_command = {
+      require('telescope').setup({
+        defaults = {
+          -- configure to use ripgrep
+          vimgrep_arguments = {
             "rg",
-            "--files",
-            "--hidden",
+            "--follow",        -- Follow symbolic links
+            "--hidden",        -- Search for hidden files
+            "--no-heading",    -- Don't group matches by each file
+            "--with-filename", -- Print the file path with the matched lines
+            "--line-number",   -- Show line numbers
+            "--column",        -- Show column numbers
+            "--smart-case",    -- Smart case search
+
+            -- Exclude some patterns from search
             "--glob=!**/.git/*",
+            "--glob=!**/.idea/*",
             "--glob=!**/.vscode/*",
-            "--glob=!**/node_modules/*"
+            "--glob=!**/build/*",
+            "--glob=!**/dist/*",
+            "--glob=!**/yarn.lock",
+            "--glob=!**/package-lock.json",
+          },
+        },
+        pickers = {
+          find_files = {
+            hidden = true,
+            find_command = {
+              "rg",
+              "--files",
+              "--hidden",
+              "--glob=!**/.git/*",
+              "--glob=!**/.vscode/*",
+              "--glob=!**/node_modules/*"
+            }
+          }
+        }
+      })
+    end
+  },
+  {
+    "nvim-telescope/telescope-ui-select.nvim",
+    config = function()
+      require("telescope").setup {
+        extensions = {
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown {
+            }
           }
         }
       }
-    })
-  end
+      require("telescope").load_extension("ui-select")
+    end
+
+  }
 }
 
