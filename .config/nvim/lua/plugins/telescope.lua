ed86fd6c6c7c7a2fd7194463647b34069dd5e9d0
Tanveer Ahmed <tanveer@test.com>
2024-12-24 21:45:42 +0530

    nvim linter for terraform

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -10,7 +10,7 @@ return {
       vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
       vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
       vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
-      vim.keymap.set('n', '<leader>fgs', builtin.git_status, { desc = 'Telescope git status' })
+      -- vim.keymap.set('n', '<leader>fgs', builtin.git_status, { desc = 'Telescope git status' })
 
       require('telescope').setup({
         defaults = {
