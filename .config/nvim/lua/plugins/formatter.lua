a0b4ff733ef5def7e8d7e3990d91fe8c42d71aae
Tanveer Ahmed <tanveer@test.com>
2024-12-28 12:53:39 +0530

    removed none-ls and added conform for formatting

---
+++ .config/nvim/lua/plugins/formatter.lua
@@ -0,0 +1,20 @@
+return {
+	"stevearc/conform.nvim",
+	opts = {},
+	config = function()
+		local conform = require("conform")
+		conform.setup({
+
+			formatters_by_ft = {
+				python = { "isort", "black" },
+				lua = { "stylua" },
+				json = { "prettier" },
+				yaml = { "prettier" },
+				markdown = { "prettier" },
+			},
+		})
+		vim.keymap.set({ "n", "v" }, "<leader>gf", function()
+			conform.format({})
+		end, { desc = "Format file or range (in visual mode)" })
+	end,
+}
--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,46 +1,71 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = {
-          "lua_ls",
-          "terraformls",
-          "pyright",
-          "jsonls",
-        },
-      })
-    end,
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      -- The nvim-cmp almost supports LSP's capabilities so You should advertise it to LSP servers..
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls",
+					"terraformls",
+					"pyright",
+					"jsonls",
+				},
+			})
+		end,
+	},
+	{
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		config = function()
+			require("mason-tool-installer").setup({
 
-      local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.pyright.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.terraformls.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.jsonls.setup({
-        capabilities = capabilities,
-      })
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-    end,
-  },
+				-- a list of all tools you want to ensure are installed upon
+				-- start
+				ensure_installed = {
+
+					-- you can pin a tool to a particular version
+					-- { "golangci-lint", version = "v1.47.0" },
+					--
+					-- -- you can turn off/on auto_update per tool
+					-- { "bash-language-server", auto_update = true },
+					"stylua",
+					"shfmt",
+					"pylint",
+					"tflint",
+					"isort",
+					"prettier",
+          "black"
+				},
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			-- The nvim-cmp almost supports LSP's capabilities so You should advertise it to LSP servers..
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.pyright.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.terraformls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.jsonls.setup({
+				capabilities = capabilities,
+			})
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+		end,
+	},
 }
